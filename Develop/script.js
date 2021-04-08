9950c24b4435846db5bf9f23abce48f5ca489953
Matt Linder <mjlinder218@gmail.com>
2021-04-08 13:26:07 -0700

    Added basic prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,43 @@
+
+
+// var length = 128;
+// var lowercase = true;
+// var uppercase = true;
+// var numeric = true;
+var special = true;
+// var requirements = [length, ]
+var specialCharacters =  " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+console.log(specialCharacters);
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+
+// Write password to the #password input - FUNCTION (2)
 function writePassword() {
   var requirements = gatherRequirements();
   var password = generatePassword(requirements);
+  
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// Add EVENT LISTENER to generate button - START HERE (1)
 generateBtn.addEventListener("click", writePassword);
 
 
-// *******User selection gatherer
+// ***User selection gatherer - FUNCTION** (3)
 function gatherRequirements() {
-  var input = prompt("A banner with the strange device...", "What was it?");
-  return input;
+  var length = prompt("Enter password length, from 8 to 128 characters", "Password length");
+  var lowercase = prompt("Do you want lowercase characters in the password?", "Yes or No");
+  var uppercase = prompt("Do you want uppercase characters in the password?", "Yes or No");
+  var numeric = prompt("Do you want numeric characters in the password?", "Yes or No");
+  var numeric = prompt("Do you want special characters in the password?", "Yes or No");
+  return "blah...";
 }
 
-// ****Password generator
+// ****Password generator - FUNCTION** (4)
 function generatePassword(x) {
   console.log("********");
   return x;
