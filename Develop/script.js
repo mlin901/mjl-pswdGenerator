e6be991b45e8a30f630baa47f3367e3c15c7f711
Matt Linder <mjlinder218@gmail.com>
2021-04-10 21:46:09 -0700

    Fixed bug with Cancel button handling

--- Develop/script.js
+++ Develop/script.js
@@ -6,82 +6,79 @@ var numerals = "0123456789";
 var specialCharacters =  " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 //Initial array of character sets
 var charSets = [lowerCharacters, upperCharacters, numerals, specialCharacters];
-
-// Variable for button (for event listerner below)
+// Variable for button (for event listener below)
 var generateBtn = document.querySelector("#generate");
 
 // Function called by event listener to write password to the #password input
 function writePassword() {
-  var requirements = gatherRequirements(); //Calls function to prompt user for settings
-  var password = generatePassword(requirements, charSets); //Passes user settings and character sets to password generator
-  var passwordText = document.querySelector("#password"); // Gets password from  
-  passwordText.value = password; 
+  var passwordText = document.querySelector("#password"); // Get HTML element to add password to
+  var requirements = gatherRequirements(); //Call function to prompt user for settings
+  // If the user hasn't selected any class or character for the password...
+  if (requirements == null) {
+    passwordText.value = "Operation cancelled!!!!!!!"
+  // Else, call the password generator function, passing the user's selected character types and
+  // the array of possible character sets (all possible character sets--not just user-chosen sets)
+  } else {
+    var password = generatePassword(requirements, charSets); 
+    passwordText.value = password; 
+  }
 }
 
 // Event listener for the generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
 
+// Function for yes/no questions (kinds of characters to include)
+function yesNoQuestion (option) {
+  var answer = "neither";
+  while ((answer !== "yes") && (answer != "no")) {
+    answer = prompt("Do you want " + option + " characters in the password?", "Yes or No");
+    if (answer == null) {
+      return null;
+    } else {
+      answer = answer.toLowerCase();
+      if ((answer != "yes") && (answer != "no")) {
+        alert("Invalid input. Enter Yes or No.");
+      } else {
+        return answer;
+      }
+    }
+  }
 
-// User selection gatherer - FUNCTION** (3)
+}
+// Prompt/process user selections
 function gatherRequirements() {
   var length = 1;
   var lowercase = "lowercase";
   var uppercase = "uppercase";
-  var numeric = "mumeric";
+  var numeric = "numeric";
   var special = "special";
-  
-  // ***need to add Cancel/null possibility
+  var types = [lowercase, uppercase, numeric, special];
+
+  // Process first prompt, the one that asks for the number of characters
   // Basic idea of the following is from https://stackoverflow.com/questions/52078896/javascript-prompt-validation-for-integer-input/52079923
   while (((length < 8) || (length > 128)) || (isNaN(length))) {
     length = prompt("Input a number, from 8 to 128, which will determine the length of the password.")
-    console.log(length + " 0000000000");
     if (length == null) {
-      return "Operation cancelled."
+      return null;
     } else if (isNaN(length)) {
       alert("Invalid input. Enter a number from 8 to 128.");
     } else if ((length < 8) || (length > 128)) {
       alert("Invalid length. Must be from 8 to 128");
-    }
+    } 
+  }
+  var requirementsA = [];
+  
+  for (let index = 0; index < types.length; index++) {
+    requirementsA[index] = yesNoQuestion(types[index]);
+    if (requirementsA[index] == null) {
+      console.log("blah"); // **************************
+      return null;
+    }  
   }
   
-  function yesNoQuestion (option) {
-    var answer = "neither";
-    while ((answer !== "yes") && (answer != "no")) {
-      answer = prompt("Do you want " + option + " characters in the password?", "Yes or No");
-      if (answer == null) {
-        return null;
-      } else {
-        answer = answer.toLowerCase();
-        if ((answer != "yes") && (answer != "no")) {
-          alert("Invalid input. Enter Yes or No.");
-        } else {
-          return answer;
-        }
-      }
-    }
-
-  }
-  
-  lowercase = yesNoQuestion(lowercase);
-  if (lowercase == null) {
-    return "Operation cancelled"
-  } 
-  uppercase = yesNoQuestion(uppercase);
-  if (uppercase == null) {
-    return "Operation cancelled"
-  } 
-  numeric = yesNoQuestion(numeric);
-  if (numeric == null) {
-    return "Operation cancelled"
-  } 
-  special = yesNoQuestion(special);
-  if (special == null) {
-    return "Operation cancelled"
-  } 
-
-  var requirementsA = [length, lowercase, uppercase, numeric, special]
+  requirementsA.unshift(length);
   return requirementsA;
-}
+} 
 
 // ****Password generator - FUNCTION** (4)
 function generatePassword(settings, characterSets) {
@@ -89,8 +86,7 @@ function generatePassword(settings, characterSets) {
   var pswd = [];
 
   // Build up array of user-selected character sets
-  for (i = 1; i < settings.length; i++) {
-    console.log(settings[i] + "lskdfja;lsdkfjas;dlfk");
+  for (i = 1; i < settings.length; i++){   
     var newI = i - 1;
     if (settings[i] == "yes") {
       newset.push(characterSets[newI]);
